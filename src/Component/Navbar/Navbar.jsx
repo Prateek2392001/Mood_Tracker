de05d76153b8a7cfdc697a681cbb6f5f99d2bfd0
Prateek2392001 <103823442+Prateek2392001@users.noreply.github.com>
2025-01-17 10:43:44 +0530

    change login button

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 import "./Navbar.css";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <div className="navbar-logo">Daily Mood Tracker</div>
-      <div className="navbar-links">
-        <a href="#home">Home</a>
-        <a href="#reports">Reports</a>
-      </div>
-      <button className="logout-btn">Logout</button>
-    </nav>
+    <>
+      <nav className="navbar">
+        <div className="navbar-logo">Daily Mood Tracker</div>
+        <div className="navbar-links">
+          <a href="#home">Home</a>
+          <a href="#reports">Reports</a>
+        </div>
+        {/* <button className="logout-btn">Logout</button> */}
+        <button className="logout-btn">
+          <Link to="/login">Login</Link>
+        </button>
+      </nav>
+    </>
   );
 };
 
